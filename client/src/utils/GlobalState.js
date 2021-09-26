16866d17cc88cf5c862d35d1b200fde7655dc43b
JazmyneB <bradleyj1@email.arizona.edu>
2021-09-26 00:20:50 -0700

    intial setup and added actions and reducers

---
+++ client/src/utils/GlobalState.js
@@ -0,0 +1,25 @@
+import React, { createContext, useContext } from "react";
+import { useProductReducer } from './reducers';
+
+const StoreContext = createContext();
+const { Provider } = StoreContext;
+
+
+const StoreProvider = ({ value = [], ...props }) => {
+    //state -> most up to date version of global state object
+    //dispatch -> method used to execute to update our state
+    const [state, dispatch] = useProductReducer({
+      products: [],
+      categories: [],
+      currentCategory: '',
+    });
+    // use this to confirm it works!
+    console.log(state);
+    return <Provider value={[state, dispatch]} {...props} />;
+};
+
+const useStoreContext = () => {
+    return useContext(StoreContext);
+};
+
+export { StoreProvider, useStoreContext };
